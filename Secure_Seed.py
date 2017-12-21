cdd2dbe6071a14e63069d4e36624c9a45c961a22
cooperdoyle@protonmail.com <cooperdoyle@protonmail.com>
2017-12-21 11:03:46 +0000

    Update Secure_Seed.py

--- Secure_Seed.py
+++ Secure_Seed.py
@@ -1,6 +1,6 @@
+# -*- coding: utf-8 -*-
 #The following lines generate a cryptographically secure pseudorandom seed corresponding to a wallet on the IOTA network
 
-# -*- coding: utf-8 -*-
 import subprocess
 
 seed = subprocess.call("cat /dev/urandom |LC_ALL=C tr -dc ‘A-Z9’ | fold -w 81 | head -n 1", shell=True)
