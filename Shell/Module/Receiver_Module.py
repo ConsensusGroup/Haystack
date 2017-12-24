8ff37343c64ef41d05f1a5a1866ec0c6cb5bcb8c
Thomas Nommensen <thomas.nommensen@coepp.org.au>
2017-12-24 09:29:21 +0000

    Interface with Receive_Module.sh

--- Shell/Module/Receiver_Module.py
+++ Shell/Module/Receiver_Module.py
@@ -3,7 +3,11 @@ from iota.adapter.wrappers import RoutingWrapper
 from iota import *
 import sys
 
-Seed_key = "WCGOWTHOWPC9KYYDLOEDDZMUHPWVASCWPTX9PZEPWWNKNNEETCPZISMZTM99GNRCZQ9GGOBIBKNYNSPAS"
+
+
+
+Seed_key = str(sys.argv[1]) #User seed key
+
 api_rec = Iota(RoutingWrapper('https://iota.thathost.net:443').add_route('attachToTangle', 'http://localhost:14265'), seed = Seed_key)
 
 #We pull the transaction history of the account (using the seed)
@@ -14,8 +18,20 @@ balances = mess.get('balance')
 bundle = mess.get('bundles')
 Address = mess.get('addresses')
 
-print(bundle[0])
-print(Address)
-bun = bundle[0]
-message_rec = bun.get_messages(errors='drop')
-print(message_rec)
+
+message_rec=""
+for i in bundle:
+	message_rec = i.get_messages(errors='drop')
+
+message=str(message_rec)
+
+#Not a big fan of this implementation but this will have to do for now 
+#This is cleans the string of the receiving side. 
+clean = message.lstrip("[u'") 
+splitting = str(clean).split("#'#'v'#Message: ")
+splitting_Address = str(clean).split(" #'#'v'#Address: ")
+splitting_Message = splitting[1].split(" #'#'v'#Address: ")
+print(splitting_Message[0])
+print(splitting_Address[1].rstrip('"]'))
+
+
