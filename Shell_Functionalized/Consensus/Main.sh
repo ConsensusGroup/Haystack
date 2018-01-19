85536a6006b5448791a161bdf27ffe2dd632ef88
Thomas Nommensen <thomas.nommensen@coepp.org.au>
2018-01-19 09:35:11 +0000

    Update Main.sh

--- Shell_Functionalized/Consensus/Main.sh
+++ Shell_Functionalized/Consensus/Main.sh
@@ -12,21 +12,18 @@ Communication_Module="$Module/Communication_Module.sh"
 Communication_Py="$Module/Communication.py"
 iri="$current_dir/Node/iri-1.4.0.jar"
 Server="http://node.hans0r.de:14265"
-Public_Seed="SKUMGBMQIYQBHBGB9OLW9FXCHFBSWOXJWQIUAIVDZNARRRYEGGJCCNZA9KRZYWUHZOQCWBJKKRERCZEMP"
+Public_Seed="XXNCOIHEUTNGZPMQOOLUNHJFXVC9KI9LLYPVSDTQWAIQAGJGVRESTWJHKWMOYNWXNTAGVWSROWZIYLFBI"
 
 #Source the shell script with all the functions 
 source "$Communication_Module"
 
-PID=$(Node_Run "$iri" "PID")
+#PID=$(Node_Run "$iri" "PID")
 
-if [[ "$PID" == "" ]];
-then
-	#We initiate the Node using the function Node_run
-	Node_run "$iri" "Run" > /dev/null 2>&1
-fi
+#We initiate the Node using the function Node_run
+#Node_run "$iri" "Run" > /dev/null 2>&1
 
 #New User Enters the network. We need to create a folder for his data. 
-#mkdir $UserData
+mkdir $UserData
 
 #Now the user generates his Public address from his private seed.
 #Client private seed which gets saved under UserData/Seed.txt
@@ -35,22 +32,29 @@ Private_Seed=$(Seed_Address $UserData "Seed.txt" "Seed")
 #Client Public Address which is to be broadcasted to the network.
 Client_Address=$(Seed_Address $UserData "Address.txt" "Address" $Communication_Py $Private_Seed $Server)
 
-#Some public address from the public seed so that client can send current address to public ledger.
-#Address_Of_Public_Seed=$(Address_Generator $Communication_Py $Public_Seed $Server)
+for ((x=1; x<=20; x++));
+do
+	#Some public address from the public seed so that client can send current address to public ledger.
+	Address_Of_Public_Seed=$(Address_Generator $Communication_Py $Public_Seed $Server)
+	echo "Here 2 $Address_Of_Public_Seed"
 
-#We now broadcast it to the public ledger 
-#Send=$(Send_Module_Function $Communication_Py $Address_Of_Public_Seed $Private_Seed $Client_Address $Server)
+	#We now broadcast it to the public ledger 
+	Send=$(Send_Module_Function $Communication_Py $Address_Of_Public_Seed $Private_Seed $Client_Address $Server)
+	echo "Here 3: $Communication_Py $Address_Of_Public_Seed $Private_Seed $Client_Address $Server"
+	
+	#We now want to save all the public addresses on the ledger to a local file
+	Public_Addresses=$(Public_Addresses $Public_Seed $Server $Communication_Py $UserData)
+	echo "Here 4"
 
-#We now want to save all the public addresses on the ledger to a local file
-Public_Addresses=$(Public_Addresses $Public_Seed $Server $Communication_Py $UserData)
+	#Now we want to determine if we need to perform a ledger migration.
+	Dynamic_Ledger $Communication_Py $Public_Seed "2" $UserData $Server
+	echo "Here 5"
 
-#Now we want to determine if we need to perform a ledger migration.
-Dynamic_Ledger $Communication_Py $Public_Seed "2" $UserData $Server
-
-#Perform the migration.
-Data=$(Ledger_Migration $Communication_Py $UserData $Server $Public_Seed)
-echo "$Data"
-
+	#Perform the migration.
+	Public_Seed=$(Ledger_Migration $Communication_Py $UserData $Server $Public_Seed)
+	echo "$Public_Seed"
+	echo "Here 6"
+done 
 
 #Killing the app
 #PID=$(Node_run "$iri" "PID")
