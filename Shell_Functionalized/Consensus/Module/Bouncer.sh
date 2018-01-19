70a2381aca080c62dfb050f7bda5183089006976
Thomas Nommensen <thomas.nommensen@coepp.org.au>
2018-01-19 02:25:28 +0000

    Update Bouncer.sh

--- Shell_Functionalized/Consensus/Module/Bouncer.sh
+++ Shell_Functionalized/Consensus/Module/Bouncer.sh
@@ -11,7 +11,7 @@ Communication_Module="$Module/Communication_Module.sh"
 Communication_Py="$Module/Communication.py"
 iri="$current_dir/Node/iri-1.4.0.jar"
 Server="http://cryptoiota.win:14265"
-Public_Seed="RVGII9JLGCAVSJSJIFMOFKXQYVHRTKRDIEVOHDBGSS9WUV9B9ELTRJ9TFDPVSREDRBMZTQQSVJVGRQRTB" 
+Public_Seed="APSRUFSQXGO9HLSRLJEOZBOZKFTIVIBAUQXTCGBQFDOMYYM9CAGOHYZSUX9TQVFYWSXESWLGDCRMQWYXB" 
 
 #Source the shell script with all the functions 
 source "$Communication_Module"
@@ -19,15 +19,24 @@ source "$Communication_Module"
 Private_Seed="$UserData/Seed.txt"
 Current_Public_Address_Pool="$UserData/Current_Public_Address_Pool.txt"
 
-#Node_Run $iri "Run" > /dev/null 2>&1
+Node_Run $iri "Run" > /dev/null 2>&1
 
+#Bouncing 
 #while [[ "" == "" ]];
 #do
 #	Message_Being_Bounced=$(Bounce $Communication_Py $UserData $Server $Public_Seed)
 #	echo "$Message_Being_Bounced"
 #done
 
+#This tests the migration mechanism
+#First we scan the current pool 
+Message_Being_Bounced=$(Bounce $Communication_Py $UserData $Server $Public_Seed)
+echo "$Message_Being_Bounced"
 
-Seed=$(Dynamic_Ledger $Communication_Py $Public_Seed "2" $UserData $Server)
+#Now we determine the number of addresses in the pool
+Dynamic_Ledger $Communication_Py $Public_Seed "2" $UserData $Server
+
+#Migrate
+Seed=$(Ledger_Migration $Communication_Py $UserData $Server $Public_Seed)
 echo "$Seed"
 
