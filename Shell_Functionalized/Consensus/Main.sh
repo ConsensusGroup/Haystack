7ed57444b66c45a9f2056998c0e68e37c238f3b5
Thomas Nommensen <thomas.nommensen@coepp.org.au>
2018-01-19 05:43:55 +0000

    Update Main.sh

--- Shell_Functionalized/Consensus/Main.sh
+++ Shell_Functionalized/Consensus/Main.sh
@@ -12,30 +12,36 @@ Communication_Module="$Module/Communication_Module.sh"
 Communication_Py="$Module/Communication.py"
 iri="$current_dir/Node/iri-1.4.0.jar"
 Server="http://node.hans0r.de:14265"
+Public_Seed="SKUMGBMQIYQBHBGB9OLW9FXCHFBSWOXJWQIUAIVDZNARRRYEGGJCCNZA9KRZYWUHZOQCWBJKKRERCZEMP"
 
 #Source the shell script with all the functions 
 source "$Communication_Module"
 
-#We initiate the Node using the function Node_run
-#Node_run "$iri" "Run" > /dev/null 2>&1
+PID=$(Node_Run "$iri" "PID")
 
+if [[ "$PID" == "" ]];
+then
+	#We initiate the Node using the function Node_run
+	Node_run "$iri" "Run" > /dev/null 2>&1
+fi
 
-#This is a seed which will be given to all instances of the app. Eventually we add a dynamic ledger
-Public_Seed="RVGII9JLGCAVSJSJIFMOFKXQYVHRTKRDIEVOHDBGSS9WUV9B9ELTRJ9TFDPVSREDRBMZTQQSVJVGRQRTB" 
+#New User Enters the network. We need to create a folder for his data. 
+mkdir $UserData
 
-#This function looks a seed file. If not present it will generate a new seed
-Private_Seed=$(Seed_Address "$UserData" "Seed.txt" "Seed") > /dev/null 2>&1
+#Now the user generates his Public address from his private seed.
+#Client private seed which gets saved under UserData/Seed.txt
+Private_Seed=$(Seed_Address $UserData "Seed.txt" "Seed")
 
-#This function now looks for an address saved locally if not present it will generate a new public address from the Private Seed which will be sent to the Public Ledger.
-Public_Address_To_Send=$(Seed_Address $UserData "Address.txt" "Address" $Communication_Py $Private_Seed $Server)
+#Client Public Address which is to be broadcasted to the network.
+Client_Address=$(Seed_Address $UserData "Address.txt" "Address" $Communication_Py $Private_Seed $Server)
 
-#We now generate a public ledger address to which we send our public address generated from the private seed
-Public_Ledger=$(Address_Generator $Communication_Py $Public_Seed $Server)
+#Some public address from the public seed so that client can send current address to public ledger.
+Address_Of_Public_Seed=$(Address_Generator $Communication_Py $Public_Seed $Server)
 
-#Now we send our "First generated address" (i.e. Public_Address_To_Send) to the public ledger so that people can find the address within a pool but without any identity.
-Sending=$(Send_Module_Function $Communication_Py $Public_Ledger $Private_Seed $Public_Address_To_Send $Server)
+#We now broadcast it to the public ledger 
+Send=$(Send_Module_Function $Communication_Py $Address_Of_Public_Seed $Private_Seed $Client_Address $Server)
 
-#Now we check the bundles within the public ledger and check to see if our Public_Address_To_Send address is in the public ledger so others can find a contact. 
+#We now want to save all the public addresses on the ledger to a local file
 Public_Addresses=$(Public_Addresses $Public_Seed $Server $Communication_Py $UserData)
 
 
