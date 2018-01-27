0087d88cc78d5740076915a368302e65ce315afc
Thomas Nommensen <thomas.nommensen@coepp.org.au>
2018-01-27 07:42:20 +0000

    Update Bouncer.sh

--- Shell_Functionalized/Consensus/Module/Bouncer.sh
+++ Shell_Functionalized/Consensus/Module/Bouncer.sh
@@ -1,30 +1,77 @@
 #!/bin/sh
 #!/bin/python
 
-# Required directories for the script.
-current_dir="$HOME/IOTA/Consensus/"
-cd $current_dir
+#######################################################################################################
+#######################################################################################################
+######################## Message Bouncing script with the encryption ##################################
+#######################################################################################################
+#######################################################################################################
+
+#These are the required directories which we need to have:
+#root="$HOME/IOTA/Consensus/"	#This is the root but it will be input $1
+
+#Change to the root dir
+#cd $root
+
+#Make it the current working dir 
 current_dir=$PWD
+
+#Define the individual directories 
 Module="$current_dir/Module"
 UserData="$current_dir/UserData"
+
+mkdir $UserData
+
+#Complete script directories
 Communication_Module="$Module/Communication_Module.sh"
 Communication_Py="$Module/Communication.py"
-iri="$current_dir/Node/iri-1.4.0.jar"
+
+#The server which will be used
 Server="http://cryptoiota.win:14265"
-Public_Seed="APSRUFSQXGO9HLSRLJEOZBOZKFTIVIBAUQXTCGBQFDOMYYM9CAGOHYZSUX9TQVFYWSXESWLGDCRMQWYXB" 
 
-#Source the shell script with all the functions 
-source "$Communication_Module"
-
+#The RSA asymmetric encryption key directory:
+RSA="$UserData/rsa_key.bin"
 Private_Seed="$UserData/Seed.txt"
-Current_Public_Address_Pool="$UserData/Current_Public_Address_Pool.txt"
 
+#Now source the shell script with all the functions
+source $Communication_Module
 
-#Bouncing 
-runtime="yes"
-while [[ "$runtime" == "yes" ]];
+#Generate the private seed
+Seed_Address $UserData "Seed.txt" "Seed"
+
+#We need to get the private seed from the user files
+Private=$(Scan_Entries $Communication_Py $Private_Seed "Read")
+
+#Generate an RSA key
+Key_Generation $Communication_Py $Private $RSA
+
+#Compose a test message which is encrypted
+Send_Message="Hello there 3 using five bounces"
+Receiver="QYFMQCBWBGLYRIRRWKZPYKSXGHCGMJCTFKPGRKXWHMFUKKDZAPQDKDHHHLEYUMVBEHIOEQIDCWKXTHJOZ"
+
+#================= Initialization ============================#
+Public_Seed="TKXBPWRHBHQDKYAHCQQTSQQBVGTDLJPZWORMF9IEYZCFDEFWSG9YIFBXWEYIBSRBKRFMBGTHWZGLLXPV9"
+
+#Generate an address from the public seed
+Address_Of_Public_Seed=$(Address_Generator $Communication_Py $Public_Seed $Server $UserData)
+
+#Generate a Personal Address + Public Key to be broadcasted to the public ledger
+Client_Address=$(Seed_Address $UserData "Address.txt" "Address" $Communication_Py $Private $Server)
+
+#We now broadcast it to the public ledger 
+Send_Module_Function $Communication_Py $Address_Of_Public_Seed $Private "$Client_Address" $Server
+
+#Check the public seed to see if the sending has worked
+Received=$(Receiver_Module_Function $Communication_Py $Public_Seed $Server)
+
+Prepare_and_Broadcast $Communication_Py "$Send_Message" $Receiver $UserData $Server "5"
+
+run="true"
+while [[ "$run" == "true" ]];
 do
-	Message_Being_Bounced=$(Bounce $Communication_Py $UserData $Server $Public_Seed)
-	echo "$Message_Being_Bounced"
-done
+	#Now generate a public pool file 
+	Public_Addresses $Public_Seed $Server $Communication_Py $UserData
 
+	Message=$(Receiver_Decryption $Communication_Py "$UserData/" $Server $RSA)
+	echo "$Message"
+done
