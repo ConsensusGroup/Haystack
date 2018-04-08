7a195f1154a9d530a9f43f8981e5f484ccf248db
woywoy123 <33817290+woywoy123@users.noreply.github.com>
2018-04-08 22:41:30 +1000

    Update main.py

--- Haystack Alpha/Objective Programmed Functions/Version 1.01/main.py
+++ Haystack Alpha/Objective Programmed Functions/Version 1.01/main.py
@@ -1,25 +1,27 @@
-from HayStackAPI import Configuration
+from HayStackAPI import *
 
 #Kivy API
 from kivy.app import App
 from kivy.lang import Builder
-from kivy.uix.label import Label
 from kivy.uix.widget import Widget
 from kivy.properties import ObjectProperty
 from kivy.uix.screenmanager import ScreenManager, Screen
-
+from kivy.uix.button import Button
+from kivy.uix.boxlayout import BoxLayout
 #OS imports
+
 from os import listdir
 
 ##### Main Logic Commands ##########
 class LogicCommands:
 	def __init__(self):
 		pass
+
 	def ReadLogin(self, UserName, Password):
-		#if UserName == Password:
-		self.Result = True
-		#else:
-		#	self.Result = False
+		if UserName == Password:
+			self.Result = True
+		else:
+			self.Result = False
 		return self
 	def ShutDown(self):
 		App.get_running_app().stop()
@@ -27,21 +29,63 @@ class LogicCommands:
 	def ReadRegister(self, UserName, Password, Password2):
 		if Password == Password2 and Password != "" and UserName != "":
 			self.data  = True
+			init = Initialization()
+			init.Build_Directory()
+			init.Password = Password
+			init.Build_Files()
 		elif Password != Password2 or Password == "":
 			self.data = "PassError"
 		elif UserName == "":
 			self.data = "UserError"
 		return self
 
+	def Search(self, List, Element):
+		matches = []
+		for i in List:
+			if str(Element) in i and i not in matches and str(Element) != "":
+				matches.append(i)
+		return matches
 
+	def ReadInput(self, Text, Function):
+		self.Found = []
+		if Function == "Contact":
+			List = ["Cooper", "Cooper", "Kevin", "Hendrik", "Samim"]
+			self.Found = self.Search(List = List, Element = Text)
+		return self
 
-		
-###### App windows #################
+	def On_Type(self):
+		for i in self.History:
+			self.ids.ContactsList.remove_widget(i)
+
+		for i in self.Found:			
+			self.btn = Button(text = i, size_hint_y = None, height=10)
+			self.btn.bind(on_press = self.btn_pressed)
+			self.ids.ContactsList.add_widget(self.btn)
+			self.History.append(self.btn)	
+		return self
+
+	def btn_pressed(self, instance):
+	    print(instance.text)
+
+	def Message_Inbox(self):
+		List = ["Cooper", "Cooper", "Kevin", "Hendrik", "Samim"]
+		for i in List:
+			self.btn = Button(text = i, size_hint_y = None, height=10)
+			self.btn.bind(on_press = self.btn_pressed)
+			self.ids.MessageList.add_widget(self.btn)
+			self.History.append(self.btn)	
+		return self
+
+######### Screens ##########
+#------ Login Windows -----------#
 class LoginWindow(Screen):
 	def __init__(self, **kwargs):
 		super(LoginWindow, self).__init__(**kwargs)
 		self.Login = LoginWidget()
 		self.add_widget(self.Login)
+
+class LoginWidget(Widget, LogicCommands):
+	pass
 
 class RegisterWindow(Screen):
 	def __init__(self, **kwargs):
@@ -49,30 +93,36 @@ class RegisterWindow(Screen):
 		self.Register = RegisterWidget()
 		self.add_widget(self.Register)
 
+class RegisterWidget(Widget,LogicCommands):
+	pass
+
+#------ Messanger Windows -----------#
 class MessangerWindow(Screen):
 	def __init__(self, **kwargs):
 		super(MessangerWindow, self).__init__(**kwargs)
 		self.Messanger = MessangerWidget()
 		self.add_widget(self.Messanger)
 
-######################################
+class MessangerWidget(BoxLayout, LogicCommands):
+	def __init__(self):
+		BoxLayout.__init__(self)
+		LogicCommands.__init__(self)
+		self.History = []
+		self.Message_Inbox()
+
+class NewMessageWindow(Screen):
+	def __init__(self, **kwargs):
+		super(NewMessageWindow, self).__init__(**kwargs)
+		self.NewMessage = NewMessageWidget()
+		self.add_widget(self.NewMessage)
 
 
-
-######### Screens ##########
-#------ Login Windows -----------#
-class LoginWidget(Widget, LogicCommands):
-	pass
-
-class RegisterWidget(Widget,LogicCommands):
-	pass
-
-#------ Messanger Windows -----------#
-class MessangerWidget(Widget, LogicCommands):
-	pass
-
+class NewMessageWidget(Widget, LogicCommands):
+	def __init__(self):
+		Widget.__init__(self)
+		LogicCommands.__init__(self)
+		self.History = []
 ##################################
-
 
 
 class KivyConfig:
@@ -88,6 +138,8 @@ class KivyConfig:
 		self.sm.add_widget(LoginWindow(name = "Login"))
 		self.sm.add_widget(RegisterWindow(name = "Register"))
 		self.sm.add_widget(MessangerWindow(name = "Messanger"))
+
+		self.sm.add_widget(NewMessageWindow(name = "NewMessage"))
 		return self
 
 ###### Build Main ########
