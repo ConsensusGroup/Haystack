4bb4890cbef1841028250e43a6dcd54b122ff1ac
woywoy123 <33817290+woywoy123@users.noreply.github.com>
2019-02-16 03:02:07 +1100

    Update Configuration_Module.py

--- Haystack Alpha/Reworked/1.00/Configuration_Module.py
+++ Haystack Alpha/Reworked/1.00/Configuration_Module.py
@@ -4,12 +4,12 @@
 
 class Configuration:
 	def __init__(self):
-		self.Node = "http://localhost:14265"
+		self.Node = "https://nodes.thetangle.org:443"
 		self.PublicSeed = "MKHYA9FZ9STCQGNUIHFVXAAKTP9LSDNEUDSBSBXVQFRTYEMBEMKFIPMHKULCMIIWVGDWWGXTQAKOYGDOW"
-		self.Charlib = '.ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890+/-=% '
-		self.Default_Size = 234
+		self.Charlib = "0123456789qwertzuiopasdfghjklyxcvbnmQWERTZUIOPASDFGHJKLYXCVBNM^!$%&/\()=?+*~'#-_.:;,<>|@{[]} "
+		self.Default_Size = 128
 		self.Identifier = "////"
-		self.MessageIdentifier = "\\\\"
+		self.MessageIdentifier = ">>>>"
 		self.Password = "Hello world"  #This can be removed later. This is used password protect the key.
 		self.UserFolder = "a/UserData"
 		self.KeysFolder = "Keys"
@@ -18,7 +18,10 @@ class Configuration:
 		self.PrivateKey = "PrivateKey.pem"
 		self.PublicKey = "PublicKey.pem"
 		self.GenesisTime = 1520726570370
-		self.BlockTime = 1000000000000000000
+		self.BlockTime = 100000000
 		self.LowerBound = 0.9 #Percentage of block cycle completion. 
 		self.RefreshRate = 4 #This is in seconds.
+		self.MaxBounce = 1
+
+
 
