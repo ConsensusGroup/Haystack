440d1ca15713d0de07ce8b2dcef28a2ff9d1e5f0
woywoy123 <33817290+woywoy123@users.noreply.github.com>
2019-02-20 00:11:59 +1100

    Update PlayGround.py

--- Haystack Alpha/Reworked/PlayGround.py
+++ Haystack Alpha/Reworked/PlayGround.py
@@ -13,44 +13,52 @@ c.User_Modules.User_Profile()
 d.User_Modules.User_Profile()
 e.User_Modules.User_Profile()
 
-DLP = [['FTIJSEPGZJTHGGDXOXT9FYMJUY9JCFNRKIZ9RDWWFGLGOKROSDSAHLFUWHMBUOY9UAI99AWBVQTDNZHDC', '-----BEGIN PUBLIC KEY-----\nMIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAm9s8YLJpBvxlrgDWens6\n05V5bOPe4LXm/dLWRBD8p/axYGH6fBUexGKhGsY662FDJYqtWw+uQFFqmbo/3HLF\nceoEJEvUAu2g3FHcDv8Dir63BzsZATgwSlC+iwTZHj/PnrJhhPmzFj8+e/OgQ+Eq\npLY5CtafhZBg2Mg/TKdGRw9eJilmPrEF3i/9XZqnBwlszs8EUOmM2NTrZ/+gargf\nwjbIyzbFcdyPfEugNf1O/rXxaLiNCvC8cxD+NC41OEKEYA3mBT5bYx6J0Clz/any\nrZ/RAGJe4AqHd4A9AF0q54YDw4sg8w0+4FlJzYl7siyOudx2faJ6ie+EqpV9305t\nOQIDAQAB\n-----END PUBLIC KEY-----'], ['JQQIRSWTAUFTSJBFDAKHBDFRVZWDNQUSFJPIIFBHRVTNQPCMBHIB9AAYZHGOZSKMWDMZL9PMUODYYIFB9', '-----BEGIN PUBLIC KEY-----\nMIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEApxEpt0Sclfw+Z4R+IqMW\nndwu7OyWimrEJLJEphR8SDQBItNx2uSWhXgI6ySwGyh7EztntYCip/SWs0LE5Hth\nIBZPzjQTaY5Bi8AS+YdiJB7oUlzqiIscrJ8eP+bHLkHfy5Jvi/VMboJSxLL6ryLF\nIKhdQBdFG60JZQmCMjtj4AbyrHcocJx/XDxMtC6AItMwf4b3sApR/ViZqd1YLC44\nSdRLF2tgHRBa7Ev2SCAKmU4Fk6RgpGIGfQ8hsdyMYSEOYh3ZCUGGWcfX5przm7Ge\nOjVkH10Aeb6siYBQFE9btoC2hKANXEfT2YQJa5+7Rfd2/8ocrHnlYaeBEtDBes+L\nkQIDAQAB\n-----END PUBLIC KEY-----'], ['USNDGA9CJEIESTCULICQLOSNYEVTVRNUUGLVJVPTQFJGVLG9PLFPCUEWFKRRUG9BXMNJGTZMQWIEWTR9B', '-----BEGIN PUBLIC KEY-----\nMIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAsMLSbZi7vyJIT+CjdIRp\n1J2lnVenKAk/Yusk3rh/iHAC65yd90qAUa0szRhOyJ1wjNJdRNj19DuOphcSZXuU\nvF8CoUdRyV0ks4OhELXaC6uUgNcP0l9WydnxYPubIs0swKRDEmxYAanJG5rYrkhW\nVh6W7i1jr9ZRg4AnqA8FPVSw3OzqYR+Hf/CmxVOAmDeOm9dYjZMuz9BkpbdIAfu1\nKOZFjOyKbzygGGMEIHH2UOMSpL8nlzaKFtpX3etqw+mUoE5ml6NrFgaMVEhHaQOA\nKoZ4UcbUqgOP+rOZE4+IujRGcW5OEx/lz+hRJXuzcRHsOJnLZihupN7UkqXfqUQ8\neQIDAQAB\n-----END PUBLIC KEY-----'], ['WWNXOWXSNPKRFGQAMMVMTZIFKP9DOMLUOILKQUNTWCQQWVLAXJBVFRXFNZBWMOUCURDWR9V9MIWOSAVUZ', '-----BEGIN PUBLIC KEY-----\nMIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAyLHUcr30T4djkyw7AqUw\n12PVR1RWI8caco5g/pgTPdoaXTuviB7oW8fV8s+DFTiGaBnj5u7rwm6t+mgGQHyN\nGbivaGHlHRRjwQEoTMOPidDnNGhsQdGDacbNJd5jEYbwQF7uuEF6Dg2+EO1qwaW+\nAkOu6yzIzNsMiiVayfQSYwXtJuyMCpd63GTDqr4N5faJXemdyP5Sffa/qFE6cWX9\nKU9EtH9RfkHBv5dMCXuS4/L0aDegbxs8jcO1bxwnAf/wok+X+XGU2EDSCzygpfEq\nU3bj9yJP9k1Q34NpByU+GL91rHTZeam4lUyNtJNQtHvx1iPRhMX/Nprnrph2Pay8\n0wIDAQAB\n-----END PUBLIC KEY-----'], ['NNBGURPUGATZEUQUTYBSWNLXOCJXPUVM9X9LSCRTAIOBRXPEQ9YZUFSYGFYQRKIWLPMMGSFBZQJWQGGQB', '-----BEGIN PUBLIC KEY-----\nMIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAyCcWZGMN/o3qGNfV3elU\nAgFx73sXqof3OvBhfxDuZK117QPrsVuYyTu2bDcmSu61FHH9DJi/MsloV6UrfN6h\ntH+OFEdvHrmV6IKXQeX/27xsREg/iI1yelEZyUaDcA6pl5xtOtCrJ6AUXcM/xFR6\n4pbD+DxwFBv4uIpmiIqGcm8ORnnLCjrev8mfPcXMIdDOVMr70+Wc4VrSwSGHx11Y\nb0gPPuSFAEAd89b69HbcYXeiObCtRn6hvLmauKtJkZmchXFK9qjyPUrPPg+lMbD6\ndGU56E6dfqmCdGFrvwEiu+78xaNM7x5fXHke6FavsZ1+TX87jg76vRZSyMwVoGzS\newIDAQAB\n-----END PUBLIC KEY-----']]
-
-
-#for i in Nodes:
+DLP = [['YFDHFULLCJCRNEKRLU9PUGPVFOECSWCAOYJSBRRQRNTXDQHAXPRGAAAWYPJNTUGCAVFYDNIATOGWVHJSZ', '-----BEGIN PUBLIC KEY-----\nMIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAm9s8YLJpBvxlrgDWens6\n05V5bOPe4LXm/dLWRBD8p/axYGH6fBUexGKhGsY662FDJYqtWw+uQFFqmbo/3HLF\nceoEJEvUAu2g3FHcDv8Dir63BzsZATgwSlC+iwTZHj/PnrJhhPmzFj8+e/OgQ+Eq\npLY5CtafhZBg2Mg/TKdGRw9eJilmPrEF3i/9XZqnBwlszs8EUOmM2NTrZ/+gargf\nwjbIyzbFcdyPfEugNf1O/rXxaLiNCvC8cxD+NC41OEKEYA3mBT5bYx6J0Clz/any\nrZ/RAGJe4AqHd4A9AF0q54YDw4sg8w0+4FlJzYl7siyOudx2faJ6ie+EqpV9305t\nOQIDAQAB\n-----END PUBLIC KEY-----'], ['KZLRVUQZ9PCSPRHNPXVPVYPDLOUKIXMTGCNVQBBHVRESSHHIMMBEMUUUCPTNOPAIASWPBBVWWWVPCSYCX', '-----BEGIN PUBLIC KEY-----\nMIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEApxEpt0Sclfw+Z4R+IqMW\nndwu7OyWimrEJLJEphR8SDQBItNx2uSWhXgI6ySwGyh7EztntYCip/SWs0LE5Hth\nIBZPzjQTaY5Bi8AS+YdiJB7oUlzqiIscrJ8eP+bHLkHfy5Jvi/VMboJSxLL6ryLF\nIKhdQBdFG60JZQmCMjtj4AbyrHcocJx/XDxMtC6AItMwf4b3sApR/ViZqd1YLC44\nSdRLF2tgHRBa7Ev2SCAKmU4Fk6RgpGIGfQ8hsdyMYSEOYh3ZCUGGWcfX5przm7Ge\nOjVkH10Aeb6siYBQFE9btoC2hKANXEfT2YQJa5+7Rfd2/8ocrHnlYaeBEtDBes+L\nkQIDAQAB\n-----END PUBLIC KEY-----'], ['QQGRVLNCLM9CEG9GADAT9NAJUNVVYUC9Q9SDLZWEMJLSIWTXWHNZWBXIWBLTZWTCPI9FMC9RJQRLUGLJY', '-----BEGIN PUBLIC KEY-----\nMIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAsMLSbZi7vyJIT+CjdIRp\n1J2lnVenKAk/Yusk3rh/iHAC65yd90qAUa0szRhOyJ1wjNJdRNj19DuOphcSZXuU\nvF8CoUdRyV0ks4OhELXaC6uUgNcP0l9WydnxYPubIs0swKRDEmxYAanJG5rYrkhW\nVh6W7i1jr9ZRg4AnqA8FPVSw3OzqYR+Hf/CmxVOAmDeOm9dYjZMuz9BkpbdIAfu1\nKOZFjOyKbzygGGMEIHH2UOMSpL8nlzaKFtpX3etqw+mUoE5ml6NrFgaMVEhHaQOA\nKoZ4UcbUqgOP+rOZE4+IujRGcW5OEx/lz+hRJXuzcRHsOJnLZihupN7UkqXfqUQ8\neQIDAQAB\n-----END PUBLIC KEY-----'], ['IGQ9PSJYCDJKIXHLDNEIQE99GJLKIEJQFTUWSEITUFWMHTTWKVHNTMUJGLUNMQUSOLGYWABUWYPHSEGRX', '-----BEGIN PUBLIC KEY-----\nMIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAyLHUcr30T4djkyw7AqUw\n12PVR1RWI8caco5g/pgTPdoaXTuviB7oW8fV8s+DFTiGaBnj5u7rwm6t+mgGQHyN\nGbivaGHlHRRjwQEoTMOPidDnNGhsQdGDacbNJd5jEYbwQF7uuEF6Dg2+EO1qwaW+\nAkOu6yzIzNsMiiVayfQSYwXtJuyMCpd63GTDqr4N5faJXemdyP5Sffa/qFE6cWX9\nKU9EtH9RfkHBv5dMCXuS4/L0aDegbxs8jcO1bxwnAf/wok+X+XGU2EDSCzygpfEq\nU3bj9yJP9k1Q34NpByU+GL91rHTZeam4lUyNtJNQtHvx1iPRhMX/Nprnrph2Pay8\n0wIDAQAB\n-----END PUBLIC KEY-----'], ['TPPHJQRVIISBITWWQMOOVMZLDI9IAKSLTAMGLMCNWIYRZDLRZHCKYGDRIFPAVEQTCJTGTHNVUAQRKMYAD', '-----BEGIN PUBLIC KEY-----\nMIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAyCcWZGMN/o3qGNfV3elU\nAgFx73sXqof3OvBhfxDuZK117QPrsVuYyTu2bDcmSu61FHH9DJi/MsloV6UrfN6h\ntH+OFEdvHrmV6IKXQeX/27xsREg/iI1yelEZyUaDcA6pl5xtOtCrJ6AUXcM/xFR6\n4pbD+DxwFBv4uIpmiIqGcm8ORnnLCjrev8mfPcXMIdDOVMr70+Wc4VrSwSGHx11Y\nb0gPPuSFAEAd89b69HbcYXeiObCtRn6hvLmauKtJkZmchXFK9qjyPUrPPg+lMbD6\ndGU56E6dfqmCdGFrvwEiu+78xaNM7x5fXHke6FavsZ1+TX87jg76vRZSyMwVoGzS\newIDAQAB\n-----END PUBLIC KEY-----']]
+#Submitting users to ledger for testing the DLP
+#	^^^^^^^^for i in Nodes:
 #	start = eval(i).Haystack_Module.Dynamic_Public_Ledger()
 #	start.Start_Ledger()
 #	print(start.Ledger_Accounts)
 #	print(i)
+#print(start.Ledger_Accounts)
 
+
+#Testing the shrapnelling and reconstruction functions 
 #UserA = a.Haystack_Module.Messaging_Client(Delete_Input = DLP)
 #Output = UserA.Shrapnell_Function(Message_PlainText = "Hello this is thomas and I am testin I am testin I am testin I am testin I am testin I am testin I am testing shit out")
 #UserA.Rebuild_Shrapnells(String = Output)
 
+#Testing the transmission channel
 UserA = a.Haystack_Module.Messaging_Client(Delete_Input = DLP)
-Output = UserA.Sending_Message(Message_PlainText = "0"*256, ReceiverAddress = DLP[0][0], PublicKey = DLP[0][1])
+Output = UserA.Sending_Message(Message_PlainText = "Hello world", ReceiverAddress = DLP[3][0], PublicKey = DLP[3][1])
 
-'''
-print("################################################################################################")
-PersonA = a.Haystack_Module.Messaging_Client(Delete_Input = DLP)
-Output = PersonA.Receiving_Message(CipherText = Output[0])
-print(Output.MessageShrapnells)
-print("################################################################################################")
-PersonB = b.Haystack_Module.Messaging_Client(Delete_Input = DLP)
-Output = PersonB.Receiving_Message(CipherText = Output.ToRelay[0][1])
-print(Output.MessageShrapnells)
-print("################################################################################################")
-PersonC = c.Haystack_Module.Messaging_Client(Delete_Input = DLP)
-Output = PersonC.Receiving_Message(CipherText = Output.ToRelay[0][1])
-print(Output.MessageShrapnells)
-print("################################################################################################")
-PersonD = d.Haystack_Module.Messaging_Client(Delete_Input = DLP)
-Output = PersonD.Receiving_Message(CipherText = Output.ToRelay[0][1])
-print(Output.MessageShrapnells)
-print("################################################################################################")
-PersonE = e.Haystack_Module.Messaging_Client(Delete_Input = DLP)
-Output = PersonE.Receiving_Message(CipherText = Output.ToRelay[0][1])
-print(Output.MessageShrapnells)
-'''
+#UserA = a.Haystack_Module.Messaging_Client().Check_Inbox()
+#Output = a.Haystack_Module.Messaging_Client().Receiving_Message(CipherText = Output[0])
+
+for i in range(15):
+	print("################################################################################################")
+	PersonA = a.Haystack_Module.Messaging_Client(Delete_Input = DLP)
+	if i == 0:
+		Output = PersonA.Receiving_Message(CipherText = Output[0])
+	else:
+		Output = PersonA.Receiving_Message(CipherText = Output.ToRelay)
+	print("PersonA")
+	print("################################################################################################")
+	PersonB = b.Haystack_Module.Messaging_Client(Delete_Input = DLP)
+	Output = PersonB.Receiving_Message(CipherText = Output.ToRelay)
+	print("PersonB")
+	print("################################################################################################")
+	PersonC = c.Haystack_Module.Messaging_Client(Delete_Input = DLP)
+	Output = PersonC.Receiving_Message(CipherText = Output.ToRelay)
+	print("PersonC")
+	print("################################################################################################")
+	PersonD = d.Haystack_Module.Messaging_Client(Delete_Input = DLP)
+	Output = PersonD.Receiving_Message(CipherText = Output.ToRelay)
+	print("PersonD")
+	print("################################################################################################")
+	PersonE = e.Haystack_Module.Messaging_Client(Delete_Input = DLP)
+	Output = PersonE.Receiving_Message(CipherText = Output.ToRelay)
+	print("PersonE")
 
 '''
 PrivA = a.User_Modules.User_Profile()
