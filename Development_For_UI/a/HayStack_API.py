ff76824e852207d76b9fde4e5c82789dbeaa3047
Thomas Nommensen <you@example.com>
2019-11-04 03:57:39 +1100

    Started working on the UI development.

---
+++ Development_For_UI/a/HayStack_API.py
@@ -0,0 +1,152 @@
+####################################################################################
+#################### This script is intended to serve as an API ####################
+####################################################################################
+
+
+from DynamicPublicLedger_Module import Dynamic_Public_Ledger
+from Contact_Module import Contact_Client
+from User_Modules import Initialization, User_Profile
+from Inbox_Module import Inbox_Manager, Trusted_Paths
+from Haystack_Module import Sender_Client, Receiver_Client
+from Configuration_Module import Configuration
+import config
+import threading
+
+class HayStack():
+	def __init__(self):
+		pass
+
+	def Get_Current_Ledger_Addresses(self):
+		self.Current_Addresses = Dynamic_Public_Ledger().Check_User_In_Ledger(Current_Ledger = True).Ledger_Accounts
+		#Output = Current addresses within the ledger pool (list)
+		self.BlockNumber = Dynamic_Public_Ledger().Calculate_Block().Block
+		#Output = Current block number since genesis (int)
+		return self
+
+	def Get_Current_Address(self):
+		self.Current_Address = Dynamic_Public_Ledger().PrivateIOTA.Generate_Address(Index = Dynamic_Public_Ledger().Calculate_Block().Block)
+		#Output = Address of client at current block height (String)
+		return self
+
+	def Find_From_PubKey(self, PublicKey):
+		self.Identities = Contact_Client().Link_Address_To_PubKey(Public_Key_To_Search = PublicKey).Other_Identities
+		#Output = [PublicKey(string), known by other addresses(list)]
+		return self
+
+	def Find_From_Address(self, Address):
+		Identities = Contact_Client().Link_Address_To_PubKey(Address_To_Search = Address).Other_Identities
+		#Output = [PublicKey(string), known by other addresses(list)]
+		return Identities
+
+	def Delete_From_Contacts(self, Username):
+		Contact_Client().Delete_From_Contacts(User_Name = Username)
+		#Output = Nothing
+		return self
+
+	def Username_From_Address(self, Address):
+		Output = Contact_Client().Retrieve_UserName_From_Address(Address_To_Search = Address)
+		#if Address in contact list; --> Output = [Saved Username (string), PublicKey(string), Has the address been found? (Bool)] otherwise a simple False(bool) will be returned
+		return Output
+
+	def Refresh_Contact_List(self):
+		Contact_Client().Update_Contacts()
+		#Output = Nothing
+		return self
+
+	def Build_All_Directories(self):
+		Initialization().Build_Application()
+		Initialization().InboxGenerator()
+		User_Profile()
+		Inbox_Manager().Create_DB()
+		Contact_Client().Build_ContactDB()
+		Trusted_Paths().Build_LedgerDB()
+		#Output = Nothing
+		return self
+
+	def Send_Message(self, Message, ReceiverAddress, PublicKey, DifferentPaths, Encrypted):
+		Output = Sender_Client().Send_Message(Message = Message, ReceiverAddress = ReceiverAddress, PublicKey = PublicKey, DifferentPaths = DifferentPaths, Encrypted = Encrypted)
+		#Output = [ReceiverAddresses, Hash of Tx, Relaying Address] (This will be a list)
+		return Output
+
+	def Ping_Function(self):
+		Sender_Client().Ping_Function()
+		#Output = Nothing
+		return self
+
+	def Inbox(self):
+		Output = Receiver_Client().Check_Inbox()
+		Incoming_Message = Output.Incoming_Message
+		#Output = If messages are addressed to client; --> Output = [Messages] (list), otherwise [[False, False, False]]
+
+		Sending_Error = Output.Error
+		#Output = If there are errors in relaying, True (Bool); else False (Bool)
+		return [Incoming_Message, Sending_Error]
+
+
+
+from time import sleep
+
+class Run_HayStack_Client(threading.Thread):
+	def __init__(self):
+		threading.Thread.__init__(self)
+		self.RunTime = True
+
+	def run(self):
+		z = 0
+		while self.RunTime:
+			z = z +1
+			self.Echo = z
+			sleep(0.5)
+
+	def Output(self):
+		return self.Echo
+
+	def Terminate(self):
+		self.RunTime = False
+		return self
+
+	######################################## Work in progress for later ##########################################
+	# def Sync_To_Lastest_Block(self):
+	# 	for i in Trusted_Paths().Catch_Up():
+	# 		print(i)
+	#
+	# def Return_Trusted_Paths(self):
+	# 	## # TODO: Write a script that reads back all the checked nodes
+	# 	return self
+	#
+	# def Start_Ledger(self):
+	# 	Dynamic_Public_Ledger().Start_Ledger()
+	# 	return self
+	#
+	# def Run_HayStack_Client(self):
+	# 	global RunTime
+	# 	RunTime = True
+	# 	self.Build_All_Directories()
+	# 	For_Ping = 1
+	# 	while RunTime == True:
+	# 		if "0" == str(float(For_Ping)/float(Configuration().Ping_Rate)).split(".")[1]:
+	# 			self.Ping_Function()
+	# 			print("Sending Ping @: "+str(For_Ping))
+	# 		For_Ping = For_Ping +1
+	# 		Dynamic_Public_Ledger().Start_Ledger()
+	# 		Trusted_Paths().Catch_Up()
+	# 		#Trusted_Paths().Scan_Paths()
+	# 		Message = Receiver_Client().Check_Inbox()
+	# 		Message = Message.Incoming_Message
+	# 		print(Message)
+	#
+	# 		for i in Message:
+	# 			try:
+	# 				if i[0] != False:
+	# 					print("Passed!"+ "\n Message From:	 " + str(i[0]) + "\n Message:	 "+ str(i[1]))
+	# 				else:
+	# 					print("No New Message for you.....")
+	# 			except:
+	# 				print("No New Message for you.")
+	# 		sleep(Configuration().RefreshRate)
+	#
+	#
+	#
+	#
+	# 	return self
+	#
---
+++ Development_For_UI/a/main.py
@@ -0,0 +1,118 @@
+####################################################################################
+################# This is the main program that will start the UI ##################
+####################################################################################
+from Tools_Module import Tools
+from Configuration_Module import Configuration
+from HayStack_API import HayStack, Run_HayStack_Client
+from User_Modules import User_Profile
+
+#Other imports
+import config
+import getpass
+import os
+
+def Welcome_Screen():
+	print("                              ####################### Welcome to the IOTA HayStack Protocol!!! ###########################")
+	print("")
+	print("")
+	print("")
+	print("""\
+
+	                                                                                                .###
+	                                                                                              ,#######*
+	                                                                                              #########
+	                                                                                              #########
+	                                                          ((      ####    .(                    #####
+	                                                        ######   ######  #####
+	                                                        ######/   ####   .###  .####
+	                                                        ,####*                  *#( .###        .######      /(.
+	                                                                    *(.              ##(        ########  .#######
+	                                                          #####   .#####  .####         ###     #######(  ########
+	                                                         #######   #####  (####  ####             ###(     ######/
+	                                               ,######   /######                 ###(  /.                    .,
+	                                               ########     ,                         ###*      *###*
+	                                               #######*                .##.                    #######    #####
+	                                                 ###,        (#####.  ######  #####            #######   #######    ,*
+	                                                      ,.     #######  ,####.  (####  ####       .###.    *######  ######.
+	                                                   #######   /#####                  (##                    .     #######
+	                                                   ########                                  #####       ..        #####
+	                                                   #######                                   #####     #####/
+	                                         *######     ,(/               (###*                   *.      (####.   ####
+	                                        #########                ###.   (#*              ,###.                 (#####
+	                                        #########          .##*  (##                     #####     .####        ####
+	                                         #######                        ,####.        ,.   ,        ####     (#
+	                                            .             /##(   ####    ####        ####       ##(         ####,
+	                                                           ##    ,##,                 (/       ####         (###
+	                                                           .,               #####          ###         .###,
+	                                                          ####    #####     #####          .#       ##  ###
+	                                                           /(     *####      /#/               ##/ ###/
+	                                                            *#/                  ######        *#.
+	                                                           #####     /####.     .#######
+	                                                            ###.     ######      ######
+	                                                                       #(                #######
+	                                                              /#####       #####(       /#######*
+	                                                              /#####      #######        #######    *######
+	                                                                           #####/          (##     (########
+	                                                                    #####(         ######          #########
+	                                                                   #######        ########          #######*
+	                                                                    #####(        (#######             .
+	                                                                                    ####                                                                       """)
+def First_Usage():
+	if Tools().Check_File(File = str(Configuration().UserFolder+"/"+Configuration().KeysFolder+"/"+Configuration().PrivateKey)) == False:
+		#First time this program was executed
+		while True:
+			Password = getpass.getpass(prompt = "Enter a password: ")
+			Password2 = getpass.getpass(prompt = "Enter the passsword again: ")
+			if Password == Password2:
+				config.Password = Password
+				break
+			else:
+				print("")
+				print("Passwords do not match!")
+				print("")
+		HayStack().Build_All_Directories()
+	else:
+		while True:
+			#Turn this on later
+			#Password = getpass.getpass(prompt = "Enter the HayStack password: ", stream = None)
+			#config.Password = Password
+			try:
+				User_Profile().PrivateKey
+				break
+			except ValueError:
+				print("The password was incorrect. Please try again.")
+def Second_Screen():
+	while True:
+		User_Choice = raw_input("What would you like to do: \n a) Run the chat window\n b) Run a non interactive relaying client\n c) Quit the program \n>>> ")
+		if User_Choice == "a":
+			return True
+		elif User_Choice == "b":
+			return False
+		elif User_Choice == "c":
+			return None
+
+def Non_Interactive_Client():
+	#Clears the screen
+	clear = lambda: os.system('clear')
+	clear()
+
+	print("Press 'b' to go back")
+	instance = Run_HayStack_Client()
+	instance.start()
+	while True:
+		User_Input = raw_input(">>> ")
+		if User_Input == "b":
+			instance.Terminate()
+			break
+		else:
+			print(instance.Output())
+
+
+if __name__ == "__main__":
+	#Check if the user is using this software for the first time
+	First_Usage()
+	Outcome = Second_Screen()
+	if  Outcome == None:
+		exit()
+	elif Outcome == False:
+		Non_Interactive_Client()
